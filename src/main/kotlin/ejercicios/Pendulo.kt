1ac0b912cc091f1e3565708fe420143ce8d29840
Marco Vanegas <marco.vanegas@eltransporte.org>
2022-06-13 13:36:23 -0500

    Creación de la clase

--- src/main/kotlin/ejercicios/Pendulo.kt
+++ src/main/kotlin/ejercicios/Pendulo.kt
@@ -1,4 +1,34 @@
 package ejercicios
 
+import kotlin.math.sqrt
+
 class Pendulo {
+
+    private var l: Double = 0.0 // longitud cuerda
+    private var a: Double = 0.0 // aceleracion del pendulo
+
+    constructor() {} // constructor primario
+    constructor(l: Double, a: Double) {
+        this.l = l
+        this.a = a
+    }
+
+    // Analizadoras
+    fun getLongitud() = this.l
+    fun getAceleracion() = this.a
+
+    // Modificadoras
+    fun setLongitud(l: Double) {
+        this.l = l
+    }
+    fun setAceleracion(a: Double) {
+        this.a = a
+    }
+
+    // Métodos
+    fun calcularPeriodoOscilacion(): Double {
+        if (l == 0.0) return 0.0
+        return 2 * Math.PI * sqrt(a / l)
+    }
+
 }
